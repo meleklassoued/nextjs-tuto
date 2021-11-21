acd09889562393410318eefd6ebbdb51a3eb2c1e
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-11-21 16:18:39 +0100

    test the next Image component

--- pages/Post.js
+++ pages/Post.js
@@ -1,10 +1,13 @@
 import React from "react";
 import Link from "next/link";
+import Image from "next/image";
+import img from "../public/vercel.svg";
 
 function Post() {
   return (
     <div>
       <Link href='/'>aslema edheya el post page </Link>
+      <Image src={img} height={100} widht={144} alt='vercel' />
     </div>
   );
 }
