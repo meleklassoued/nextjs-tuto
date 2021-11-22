9a3b396297bcdc34d872a7b57318e364faf4f10e
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-11-23 00:31:00 +0100

    fetch fake api with getstatic props

--- pages/index.js
+++ pages/index.js
@@ -3,13 +3,16 @@ import Head from "next/head";
 import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 
-import { getSortedPostsData } from "../lib/posts";
+// import { getSortedPostsData } from "../lib/posts";
+// import getSortedPostsData from "../lib/TestApi";
 
 export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  console.log(res.json);
+  const data = await res.json();
   return {
     props: {
-      allPostsData,
+      data,
     },
   };
 }
@@ -17,17 +20,16 @@ export async function getStaticProps() {
 //add the Layout
 import Layout from "../components/Layout";
 
-export default function Home({ allPostsData }) {
+export default function Home({ data }) {
   return (
     <>
       <Layout>
-        {allPostsData.map(({ id, date, title }) => (
+        {data.map(({ id, title }) => (
           <li key={id}>
             {title}
             <br />
             {id}
             <br />
-            {date}
           </li>
         ))}
       </Layout>
