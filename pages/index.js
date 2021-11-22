2e8b61bf7a2b12cfccdf4f7a05e794caf5a45f75
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-11-23 00:06:57 +0100

    make the static generation

--- pages/index.js
+++ pages/index.js
@@ -2,40 +2,34 @@ import React, { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
 // import styles from "../styles/Home.module.css";
-import Post from "./Post";
-import Link from "next/dist/client/link";
-import cn from "classnames";
 
+import { getSortedPostsData } from "../lib/posts";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
 
 //add the Layout
 import Layout from "../components/Layout";
 
-export default function Home() {
- 
+export default function Home({ allPostsData }) {
   return (
     <>
       <Layout>
-        <Head>
-          <title>page1</title>
-          <link rel='shortcut icon' href='favicon.ico' type='image/x-icon' />
-        </Head>
-        <div>melek lassoued</div>
-        <style jsx>{`
-          div {
-            color: #5e5ccf;
-            padding: 0;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            width: 100vw;
-            height: 100vh;
-            border: 10px solid #00000075;
-            background: #9b9ba9;
-            border-radius: 10%;
-            font-size: 40px;
-          }
-        `}</style>
-      
+        {allPostsData.map(({ id, date, title }) => (
+          <li key={id}>
+            {title}
+            <br />
+            {id}
+            <br />
+            {date}
+          </li>
+        ))}
       </Layout>
     </>
   );
