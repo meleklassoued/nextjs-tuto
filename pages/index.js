1e180f5319157d74470af501cddbb1610e02e657
Melek Lassoued <65103260+meleklassoued@users.noreply.github.com>
2021-11-23 23:50:07 +0100

    comment the some imports

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Head from "next/head";
-import Image from "next/image";
+// import Head from "next/head";
+// import Image from "next/image";
 // import styles from "../styles/Home.module.css";
 
 // import { getSortedPostsData } from "../lib/posts";
